d8f8078042cad97c55051a3b967dc30492f82ba6
emilierummer <114100983+emilierummer@users.noreply.github.com>
2023-09-28 15:09:37 -0400

    player left and right movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float movementSpeed; // horizontal movement speed
     [SerializeField] private int jumpHeight; // number of blocks the player can jump
+    private float movementDirection; // 0 for stationary, -1 for left, 1 for right (float so it will work with joystick)
+    private const float MAX_VELOCITY = 5f; // the maximum horizontal velocity the player can have
+    private const float SLOWDOWN_SPEED = 0.99f; // how fast to slow the player down if they pass the max velocity (smaller is faster but less smooth)
 
     // Components
     private new Rigidbody2D rigidbody;
@@ -21,13 +24,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(Mathf.Abs(rigidbody.velocity.x) > MAX_VELOCITY)
+        {
+            rigidbody.velocity *= new Vector2(SLOWDOWN_SPEED, 1);
+        } else if(movementDirection != 0)
+        {
+            rigidbody.AddForce(new Vector2(movementDirection, 0));
+        }
     }
 
     // Movement Controls
     public void OnMove(InputValue value)
     {
-        Debug.Log("move");
+        movementDirection = value.Get<float>();
     }
 
     public void OnJump()
