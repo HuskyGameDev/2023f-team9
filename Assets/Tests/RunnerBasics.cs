688f0f844a749f1e2832ce0a984fa0a1e13b7f72
emilierummer <114100983+emilierummer@users.noreply.github.com>
2023-10-13 09:03:43 -0400

    functional runner tests

--- Assets/Tests/RunnerBasics.cs
+++ Assets/Tests/RunnerBasics.cs
@@ -6,38 +6,65 @@ using UnityEngine.TestTools;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-public class RunnerTests : InputTestFixture
+public class RunnerBasics : InputTestFixture
 {
     GameObject runner;
     GameObject board;
     Keyboard keyboard;
+    Mouse mouse;
 
     [OneTimeSetUp]
-    public void OneTimeSetup()
+    public void OneTimeSetUp()
     {
+        // get prefabs
         runner = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Player.prefab");
         Assert.That(runner, !Is.Null);
         board = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Board.prefab");
         Assert.That(board, !Is.Null);
+
+        // setup devices
+        foreach (InputDevice device in InputSystem.devices)
+        {
+            if (device != null) InputSystem.RemoveDevice(device);
+        }
+        Assert.That(InputSystem.devices.Count, Is.EqualTo(0));
+        keyboard = InputSystem.AddDevice<Keyboard>();
+        Assert.That(InputSystem.GetDevice<Keyboard>(), !Is.Null);
+        mouse = InputSystem.AddDevice<Mouse>();
+        Assert.That(InputSystem.GetDevice<Mouse>(), !Is.Null);
+        Assert.That(InputSystem.devices.Count, Is.EqualTo(2));
     }
 
     [SetUp]
     public override void Setup()
     {
         SceneManager.LoadScene("Scenes/AutomatedTesting");
-        base.Setup();
-        keyboard = InputSystem.AddDevice<Keyboard>();
 
-        var mouse = InputSystem.AddDevice<Mouse>();
-        Press(mouse.rightButton);
-        Release(mouse.rightButton);
+        Assert.That(InputSystem.GetDevice<Mouse>(), !Is.Null);
+        PressAndRelease(mouse.rightButton);
     }
 
     [TearDown]
     public override void TearDown()
     {
         base.TearDown();
-        keyboard = null;
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        // remove devices
+        InputSystem.RemoveDevice(mouse);
+        InputSystem.RemoveDevice(keyboard);
+
+        Assert.That(InputSystem.devices.Count, Is.EqualTo(0), () =>
+        {
+            string returnString = "not all devices disconnected (";
+            foreach (InputDevice device in InputSystem.devices)
+                returnString += device + ", ";
+            returnString += ")";
+            return returnString;
+        });
     }
 
     [Test]
@@ -57,45 +84,6 @@ public class RunnerTests : InputTestFixture
 
         Assert.That(runnerInstance.GetComponentInChildren<Rigidbody2D>().velocity.y, Is.EqualTo(0));
         Assert.That(runnerInstance.transform.GetChild(0).position.y, Is.GreaterThan(-10f));
-    }
-
-    [UnityTest]
-    public IEnumerator RunnerMovesLeftWithKeyboard()
-    {
-        GameObject runnerInstance = Object.Instantiate(runner, Vector2.zero, Quaternion.identity);
-
-        Press(keyboard.leftArrowKey);
-        yield return new WaitForFixedUpdate();
-        Release(keyboard.leftArrowKey);
-        yield return new WaitForFixedUpdate();
-
-        Assert.That(runnerInstance.transform.GetChild(0).position.x, Is.Negative);
-    }
-
-    [UnityTest]
-    public IEnumerator RunnerMovesRightWithKeyboard()
-    {
-        GameObject runnerInstance = Object.Instantiate(runner, Vector2.zero, Quaternion.identity);
-
-        Press(keyboard.rightArrowKey);
-        yield return new WaitForFixedUpdate();
-        Release(keyboard.rightArrowKey);
-        yield return new WaitForFixedUpdate();
-
-        Assert.That(runnerInstance.transform.GetChild(0).position.x, Is.Positive);
-    }
-
-    [UnityTest]
-    public IEnumerator RunnerJumpsWithKeyboard()
-    {
-        Object.Instantiate(board, Vector2.zero, Quaternion.identity);
-        GameObject runnerInstance = Object.Instantiate(runner, new Vector2(0, -9.5f), Quaternion.identity);
-
-        yield return new WaitForSeconds(0.5f);
-
-        PressAndRelease(keyboard.upArrowKey);
-        yield return new WaitForFixedUpdate();
-        Assert.That(runnerInstance.GetComponentInChildren<Rigidbody2D>().velocity.y, Is.Not.EqualTo(0));
     }
 
     [UnityTest]
---
+++ Assets/Tests/RunnerKeyboardMovement.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class RunnerKeyboardMovement : InputTestFixture
+{
+    GameObject runner;
+    GameObject board;
+    Keyboard keyboard;
+    Mouse mouse;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        // get prefabs
+        runner = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Player.prefab");
+        Assert.That(runner, !Is.Null);
+        board = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Board.prefab");
+        Assert.That(board, !Is.Null);
+
+        // setup devices
+        foreach (InputDevice device in InputSystem.devices)
+        {
+            if (device != null) InputSystem.RemoveDevice(device);
+        }
+        Assert.That(InputSystem.devices.Count, Is.EqualTo(0));
+        keyboard = InputSystem.AddDevice<Keyboard>();
+        Assert.That(InputSystem.GetDevice<Keyboard>(), !Is.Null);
+        mouse = InputSystem.AddDevice<Mouse>();
+        Assert.That(InputSystem.GetDevice<Mouse>(), !Is.Null);
+        Assert.That(InputSystem.devices.Count, Is.EqualTo(2));
+    }
+
+    [SetUp]
+    public override void Setup()
+    {
+        SceneManager.LoadScene("Scenes/AutomatedTesting");
+
+        Assert.That(InputSystem.GetDevice<Mouse>(), !Is.Null);
+        PressAndRelease(mouse.rightButton);
+    }
+
+    [TearDown]
+    public override void TearDown()
+    {
+        base.TearDown();
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        // remove devices
+        InputSystem.RemoveDevice(mouse);
+        InputSystem.RemoveDevice(keyboard);
+
+        Assert.That(InputSystem.devices.Count, Is.EqualTo(0), () =>
+        {
+            string returnString = "not all devices disconnected (";
+            foreach (InputDevice device in InputSystem.devices)
+                returnString += device + ", ";
+            returnString += ")";
+            return returnString;
+        });
+    }
+
+    [UnityTest]
+    public IEnumerator RunnerMovesLeftWithKeyboard()
+    {
+        GameObject runnerInstance = Object.Instantiate(runner, Vector2.zero, Quaternion.identity);
+
+        Press(keyboard.leftArrowKey);
+        yield return new WaitForSeconds(0.25f);
+        Release(keyboard.leftArrowKey);
+        yield return new WaitForFixedUpdate();
+
+        Assert.That(runnerInstance.GetComponentInChildren<Rigidbody2D>().velocity.x, Is.Negative);
+    }
+
+    [UnityTest]
+    public IEnumerator RunnerMovesRightWithKeyboard()
+    {
+        GameObject runnerInstance = Object.Instantiate(runner, Vector2.zero, Quaternion.identity);
+
+        Press(keyboard.rightArrowKey);
+        yield return new WaitForSeconds(0.25f);
+        Release(keyboard.rightArrowKey);
+        yield return new WaitForFixedUpdate();
+
+        Assert.That(runnerInstance.GetComponentInChildren<Rigidbody2D>().velocity.x, Is.Positive);
+    }
+
+    [UnityTest]
+    public IEnumerator RunnerJumpsWithKeyboard()
+    {
+        Object.Instantiate(board, Vector2.zero, Quaternion.identity);
+        GameObject runnerInstance = Object.Instantiate(runner, new Vector2(0, -9.5f), Quaternion.identity);
+
+        yield return new WaitForSeconds(0.5f);
+
+        PressAndRelease(keyboard.upArrowKey);
+        yield return new WaitForFixedUpdate();
+        Assert.That(runnerInstance.GetComponentInChildren<Rigidbody2D>().velocity.y, Is.Not.EqualTo(0));
+    }
+}
