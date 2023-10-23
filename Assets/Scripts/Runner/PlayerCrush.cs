8a9c9dfad348ff125baf12a42c1287fe732fa55e
emilierummer <114100983+emilierummer@users.noreply.github.com>
2023-10-23 08:45:36 -0400

    add PlayerCrush script and basic functionality

---
+++ Assets/Scripts/Runner/PlayerCrush.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerCrush : MonoBehaviour
+{
+    // Components
+    private TilemapCollider2D tilemapCollider; // the board's tilemap collider
+    private CircleCollider2D crushCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        crushCollider = this.GetComponentInChildren<CircleCollider2D>();
+        try
+        {
+            tilemapCollider = FindAnyObjectByType<TilemapCollider2D>();
+        }
+        catch
+        {
+            // do nothing, it doesn't matter if there is no tilemap for now
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (crushCollider.IsTouching(tilemapCollider))
+        {
+            Crush();
+        }
+    }
+
+    private void Crush()
+    {
+        GameManager.Instance.GameOver(false);
+    }
+}
