5c6fc1bc0e78ba08b6aad8177a76932f22cdb0ad
Han Yang <hyang4@mtu.edu>
2023-11-30 21:49:18 -0500

    falling power up (not finish yet)
    the falling power up falls through the board.

--- Assets/Scripts/PowerUp/PowerUp.cs
+++ Assets/Scripts/PowerUp/PowerUp.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PowerUp : MonoBehaviour
 {
     public PowerUpEffect powerUpEffect;
+
+    void Start()
+    {
+
+    }
+
     // trigger power up on collision
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,6 +20,24 @@ public class PowerUp : MonoBehaviour
             Destroy(gameObject);
             powerUpEffect.Apply(collision.gameObject);
         }
-        
+        if (collision.CompareTag("Board"))
+        {
+            StayOnTopOfBoard(collision.gameObject.GetComponent<Tilemap>());
+        }
+
+    }
+    private void StayOnTopOfBoard(Tilemap boardTilemap)
+    {
+        if (boardTilemap != null)
+        {
+            Vector3Int cellPosition = boardTilemap.WorldToCell(transform.position);
+            Vector3 cellCenter = boardTilemap.GetCellCenterWorld(cellPosition);
+
+            // Set the power-up's position to be just above the board
+            transform.position = new Vector3(cellCenter.x, cellCenter.y, transform.position.z);
+        }
+
+
+
     }
 }
